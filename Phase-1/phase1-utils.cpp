9f4c7456f184e19786c32fbd0a023ebae2a1489f
joxema4825 <joxema4825@gmail.com>
2025-11-05 06:21:47 +0000

    Created Edge and Graph Class

--- Phase-1/phase1-utils.cpp
+++ Phase-1/phase1-utils.cpp
@@ -1,15 +1,38 @@
 #include "phase1-utils.hpp"
 
-Node::Node(unsigned long long _id, double _lat, double _lon) {
+Node::Node(unsigned int _id, double _lat, double _lon) {
     id = _id;
     lat = _lat;
     lon = _lon;
-    };
+}
 
 void Node::update_restriction(bool _restricted) {
     restricted = _restricted;
 }
 
-void Node::update_pois(std::string place) {
-    pois.push_back(place);
+void Node::update_pois(std::string place, bool val) {
+    pois[place] = val;
+}
+
+Edge::Edge(unsigned int _id, Node* _from, Node* _to, std::string type) {
+    id = _id;
+    from = _from;
+    to = _to;
+}
+
+void Edge::update_restriction(bool _forbidden) {
+    forbidden = _forbidden;
+}
+
+void Edge::update_length(double _len) {
+    if(_len > 0) {
+        length = _len;
+    }
+    else {
+        throw std::invalid_argument("Length should be positive");
+    }
+}
+
+void Edge::update_type(std::string _type) {
+    type = _type;
 }
--- Phase-1/phase1-utils.hpp
+++ Phase-1/phase1-utils.hpp
@@ -1,27 +1,57 @@
 #include <vector>
 #include <string>
-#include <queue>
 #include <unordered_map>
 #include <utility>
 #include <algorithm>
+#include <stdexcept>
+#include <nlohmann/json.hpp>
 
 class Node {
 
 public:
-    unsigned long long id;
+    unsigned int id;
     double lat;
     double lon;
-    std::vector<std::string> pois;
+    std::unordered_map<std::string, bool> pois;
     bool restricted;
 
-    Node(unsigned long long _id, double _lat, double _lon);
+    Node(unsigned int _id, double _lat, double _lon);
 
     void update_restriction(bool _restricted);
 
-    void update_pois(std::string place);
+    void update_pois(std::string place, bool val);
+};
 
+class Edge {
+
+public:
+    unsigned int id;
+    Node* from;
+    Node* to;
+    double length;
+    bool directed;
+    bool forbidden = false;
+    double time;
+    std::string type;
+
+    Edge(unsigned int _id, Node* from, Node* to, std::string type);
+
+    void update_restriction(bool _forbidden);
+
+    void update_length(double _len);
+
+    void update_type(std::string _type);
 };
 
 class Graph {
+
+public:
+    // int N; // No. of vertices
+    std::vector<Edge*> edge_list;
+    std::vector<Node*> nodes; // is it needed?
+    std::unordered_map<Node*, std::vector<Node*>> adj_list;
+
+    // void CreateNode(unsigned int _id, double _lat, double )
+    
 
 };
